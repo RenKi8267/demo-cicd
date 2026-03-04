23b1164c11d80223031cb6b25cdea22dbd9fdfe5
huang <xiaodinghuang@swireproperties.com>
2026-03-04 15:35:43 +0800

    init a project

---
+++ src/main/java/com/example/demo/controller/TestController.java
@@ -0,0 +1,18 @@
+package com.example.demo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import lombok.extern.slf4j.Slf4j;
+
+@RestController
+@RequestMapping("/test")
+@Slf4j
+public class TestController {
+
+    @GetMapping("/ping")
+    public String ping() {
+        return "test ping";
+    }
+}
